a6479ad96b52598e38299b0f6e06a4b0d288607e
Wei Zhang <ink.wj.zhang@gmail.com>
2017-05-03 12:48:14 -0700

    Created aliases for code structure, added apple

--- game/game.js
+++ game/game.js
@@ -1,11 +1,51 @@
-//Create the renderer
-var renderer = PIXI.autoDetectRenderer(500, 800);
+//Aliases
+var Container = PIXI.Container,
+    autoDetectRenderer = PIXI.autoDetectRenderer,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
 
-//Add the canvas to the HTML document
+//Variables
+var width = 500;
+var height = 800;
+
+var stage = new Container(),
+    renderer = autoDetectRenderer(width, height);
 document.body.appendChild(renderer.view);
 
-//Create a container object called the `stage`
-var stage = new PIXI.Container();
+//Texture Cache
+loader
+  .add([
+      "assets/img/entities/basket.png",
+      "assets/img/entities/basket_bottom.png",
+      "assets/img/food/apple.png",
+      "assets/img/food/banana.png",
+      "assets/img/food/bread.png",
+      "assets/img/food/broccoli.png",
+      "assets/img/food/orange.png"
+  ])
+  .on("progress", loadProgressHandler)
+  .load(setup);
 
-//Tell the `renderer` to `render` the `stage`
-renderer.render(stage);
+//Prints loading log to consol
+function loadProgressHandler() {
+    console.log("loading");
+}
+
+//Main
+function setup() {
+    console.log("setup");
+    var catcher = new Sprite(
+      resources["assets/img/entities/basket.png"].texture
+    );
+    var apple = new Sprite(
+      resources["assets/img/food/apple.png"].texture
+    );
+
+    stage.addChild(catcher);
+    stage.addChild(apple);
+
+    //Tell the `renderer` to `render` the `stage`
+    renderer.render(stage);
+}
+
