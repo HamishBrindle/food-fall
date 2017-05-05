79a7c555deedd395072dc772c9c412005e813a25
Hamish Brindle <hamishbrindle@gmail.com>
2017-05-05 13:55:11 -0700

    Added sound effect: coin.

--- game/game.js
+++ game/game.js
@@ -1,7 +1,11 @@
 /*
-    Main file for game
-*/
+*   game.js
+*   Main file for Food Fall!
+ */
 
+
+
+// Speed of Game
 setInterval(makeFood, 1000);
 
 //Set the game's current state to `play`:
@@ -9,35 +13,15 @@ var state = play;
 
 //Animation loop
 function gameLoop() {
-    // //Loop this function 60 times per second
-    for (var i in stage.children) {
-        if(i == 0) {
-            continue;
-        }
-        var item = stage.children[i];
-        if (item.isFood) {
-            item.y += 2;
-            item.rotation += item.rotateFactor;
-            if (isCollide(catcher, item)) {
-                item.destroy();
-            }
-            else if (item.y === GAME_HEIGHT) {
-                item.destroy();
-            }
-            //returns the bounds of the basker {x x}
-        }
-    }
     requestAnimationFrame(gameLoop);
     state();
     lastTime = new Date().getTime();
-
-    //Render the stage
     renderer.render(stage);
 }
 
-
 //State definition for "playing" the game
 function play() {
+    foodCatchCollisionDetection();
     animateBackground();
     playerMovement();
 }
@@ -51,7 +35,7 @@ function makeFood() {
     newFood.anchor.y = 0.5;
     newFood.isFood = true;
     var randomBoolean = Math.random() >= 0.5;
-    if(randomBoolean) {
+    if (randomBoolean) {
         newFood.rotateFactor = Math.random() * 0.1;
     }
     else
@@ -62,8 +46,30 @@ function makeFood() {
 
 // Determine if basket and food are colliding
 function isCollide(basket, food) {
-  return !(((basket.y + basket.height) < food.y) ||
+    return !(((basket.y + basket.height) < food.y) ||
     (basket.y > (food.y + food.height)) ||
     ((basket.x + basket.width) < food.x) ||
     (basket.x > (food.x + food.width)))
+}
+
+function foodCatchCollisionDetection() {
+    // //Loop this function 60 times per second
+    for (var i in stage.children) {
+        if (i == 0) {
+            continue;
+        }
+        var item = stage.children[i];
+        if (item.isFood) {
+            item.y += 2;
+            item.rotation += item.rotateFactor;
+            if (isCollide(catcher, item)) {
+                item.destroy();
+                sound.play('coin');
+            }
+            else if (item.y === GAME_HEIGHT) {
+                item.destroy();
+            }
+            //returns the bounds of the basker {x x}
+        }
+    }
 }
---
+++ game/sound.js
@@ -0,0 +1,13 @@
+/**
+ * sound.js
+ * Sound effects and music for Food Fall!
+ *
+ * Created by Hamish Brindle on 2017-05-05.
+ */
+
+var sound = new Howl({
+    src: ['assets/sound/SFX-coin.wav'],
+    sprite: {
+        coin: [0, 3000],
+    }
+});
