b12b0faf5b8be26d5d307b8d575083fb200285bd
kregan10 <kregan89@gmail.com>
2017-05-06 12:55:42 -0700

    TO DO: ADD SPRITES TO ONE CONTAINER IN ORDER TO OPTIMIZE REFRESH     REDUCE LAG
        https://github.com/kittykatattack/learningPixi/blob/master/README.md
    
        control + f: var superFastSprites = new ParticleContainer();
    
        We will also need to put scorebar in it's own container, and add children
        to make incremental changes.

--- game/preload.js
+++ game/preload.js
@@ -1,7 +1,17 @@
 // Stage-size parameters; aspect ratio.
 var GAME_WIDTH = 800;
 var GAME_HEIGHT = 500;
+/*
+    TO DO: ADD SPRITES TO ONE CONTAINER IN ORDER TO OPTIMIZE REFRESH
+    REDUCE LAG
 
+    https://github.com/kittykatattack/learningPixi/blob/master/README.md
+
+    control + f: var superFastSprites = new ParticleContainer();
+
+    We will also need to put scorebar in it's own container, and add children
+    to make incremental changes.
+*/
 //Variables
 var maxXspeed = 50;
 var maxYspeed = 25;
