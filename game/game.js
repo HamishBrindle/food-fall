2e67ea72378d1902ee30d862a3e76e62a153f431
kregan10 <kregan89@gmail.com>
2017-05-09 15:25:00 -0700

    took changes out that worked in feature_reduce_lag branch and put it in master,  adding accelleration for food, and made a MAX_FOOD variable

--- game/game.js
+++ game/game.js
@@ -4,7 +4,7 @@
  */
 
 // Speed of Game
-setInterval(makeFood, 1000);
+setInterval(makeFood, 10);
 
 //Set the game's current state to `play`:
 var state = play;
@@ -31,22 +31,34 @@ function play() {
     addScore();
 }
 
+var foodCount = 0;
 function makeFood() {
+    const MAX_FOOD = 5;
+    if(foodCount >= MAX_FOOD) return;
     var newFoodIndex = weightedRand(fallingObjects);
     var newFood = PIXI.Sprite.fromImage('assets/img/food/' + fallingObjects[newFoodIndex].name + '.png');
-    newFood.x = getRandomInt(0, GAME_WIDTH);
+    newFood.x = getRandomInt(newFood.width, GAME_WIDTH - newFood.width);
+    newFood.y = -newFood.height;
     newFood.anchor.x = 0.5;
     newFood.anchor.y = 0.5;
     newFood.isFood = true;
-    newFood.collideOne = false;
+    newFood.velocity = 0; //10 pixels per second
+    newFood.accelerationY = 210; //
     var randomBoolean = Math.random() >= 0.5;
     if (randomBoolean) {
         newFood.rotateFactor = Math.random() * 0.1;
     }
     else
         newFood.rotateFactor = -Math.random() * 0.1;
+    ++foodCount;
 
     stage.addChild(newFood);
+}
+
+function removeFood(childToDelete) {
+    --foodCount;
+    console.log("foodCount ", foodCount);
+    stage.removeChild(childToDelete);
 }
 
 // Determine if basket and food are colliding
@@ -59,26 +71,37 @@ function isCollide(basket, food) {
 }
 
 function foodCatchCollision() {
-//Loop this function 60 times per second
+    var currtime = new Date().getTime();
+    var deltaTime = parseFloat((currtime - lastTime)/1000);
+    var childrenToDelete = [];
     for (var i in stage.children) {
-        var item = stage.children[i];
-        if (item.isFood) {
-            item.y += 4;
-            item.rotation += item.rotateFactor;
-             if (item.y > GAME_HEIGHT) {
-                item.destroy();
+        var fallingItem = stage.children[i];
+        if (fallingItem.isFood) {
+            var deltaY = fallingItem.velocity * deltaTime;
+            var deltaVy = fallingItem.accelerationY * deltaTime;
+            fallingItem.y += deltaY;
+            fallingItem.velocity += deltaVy;
+            fallingItem.rotation += fallingItem.rotateFactor;
+             if (fallingItem.y > GAME_HEIGHT) {
+                childrenToDelete.push(fallingItem);
+                fallingItem.destroy();
             }
             try {
-                if (isCollide(catcher, item)) {
-                    item.destroy();
+                if (isCollide(catcher, fallingItem)) {
+                    childrenToDelete.push(fallingItem);
+                    fallingItem.destroy();
                     sound.play('coin');
-                    scoreCount++;
+                    ++scoreCount;
                     stage.removeChild(score);
                 }
             } catch(err) {}
         }
     }
+    for (var i = 0; i < childrenToDelete.length; i++) {
+        removeFood(childrenToDelete[i]);
+    }
 }
+
 function addScore() {
       score.x = GAME_WIDTH - 100;
       score.y = GAME_HEIGHT - 50;
