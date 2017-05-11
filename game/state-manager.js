3131424ec33b9efda5218dfa1db93a1905452d7b
Hamish Brindle <hamishbrindle@gmail.com>
2017-05-10 21:09:32 -0700

    Merge branch 'feature-css-canvas'.

---
+++ game/state-manager.js
@@ -0,0 +1,22 @@
+/**
+ * Created by hamis on 2017-05-10.
+ */
+/**
+ * Created by hamis on 2017-05-10.
+ */
+
+var gamePaused = false;
+
+$("#btn-pause").click(function() {
+    if (!gamePaused) {
+        state = mainMenu;
+        gamePaused = true;
+    } else {
+        state = play;
+        gamePaused = false;
+    }
+});
+
+$("#btn-leaderboard").click(function() {
+    $('div').html('<ul>')
+});
