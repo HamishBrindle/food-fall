3180b5bcef0e07653d45beddbb744d182576006f
Wei Zhang <ink.wj.zhang@gmail.com>
2017-05-17 11:56:23 -0700

    Revert "AYYLMAO 2"

--- game/preload.js
+++ game/preload.js
@@ -1,8 +1,20 @@
-// game.Stage-size parameters; aspect ratio.
+// Stage-size parameters; aspect ratio.
 
 var GAME_WIDTH = 800;
 var GAME_HEIGHT = 500;
+var gameboundw = GAME_WIDTH;
+var gameboundh = GAME_HEIGHT;
+/*
+    TO DO: ADD SPRITES TO ONE CONTAINER IN ORDER TO OPTIMIZE REFRESH
+    REDUCE LAG
 
+    https://github.com/kittykatattack/learningPixi/blob/master/README.md
+
+    control + f: var superFastSprites = new ParticleContainer();
+
+    We will also need to put scorebar in it's own container, and add children
+    to make incremental changes.
+*/
 //Variables
 var maxXspeed = 50;
 var maxYspeed = 25;
@@ -29,10 +41,13 @@ var lastTime;
 
 //Aliases.
 var Container = PIXI.Container,
+    autoDetectRenderer = PIXI.autoDetectRenderer,
     loader = PIXI.loader,
     resources = PIXI.loader.resources,
-    Sprite = PIXI.Sprite,
-    Ticker = PIXI.ticker.Ticker;
+    Sprite = PIXI.Sprite;
+
+// Rendering Options.
+var myView = document.getElementById('myCanvas');
 
 var rendererOptions = {
     antiAliasing: false,
@@ -41,41 +56,43 @@ var rendererOptions = {
     autoResize: true
 };
 
-// Create game.renderer.
-var game = new PIXI.Application(GAME_WIDTH, GAME_HEIGHT, rendererOptions);
+// Create renderer.
+var renderer = autoDetectRenderer(GAME_WIDTH, GAME_HEIGHT, myView, rendererOptions);
 
-// game.Renderer position on screen.
-game.renderer.view.style.position = "absolute";
-game.renderer.view.style.top = "0px";
-game.renderer.view.style.left = "0px"; // Centers window.
+// Create new Container for stage.
+var stage = new Container();
 
-// Add game.renderer to page.
-document.getElementById("game-window").appendChild(game.view);
+// Renderer position on screen.
+renderer.view.style.position = "absolute";
+renderer.view.style.top = "0px";
+renderer.view.style.left = "0px"; // Centers window.
+
+// Add renderer to page.
+document.getElementById("game-window").appendChild(renderer.view);
 
 //Globals -------------------------------------------------------------------------------Globals
-
-// Game playing state globals
 var catcher;
+
 var tk;
-var scale = scaleToWindow(game.renderer.view);
+
+var scale = scaleToWindow(renderer.view);
+
 var setupdone = false;
+
 var pointer;
 
-// Main menu globals
-var gameBuild = true;
+gameBuild = true;
+
 var playButton;
+
 var menuBuild;
+
 var logo;
+
 var catcherBuild;
 
-// Easter egg related globals
-var easterEggEnabled = false;
-var items = [];
-var outputSprite;
-var currentTexture;
-var renderTexture2;
-var renderTexture;
-var stuffContainer;
+// Texture Cache
+loadBackgroundTextures();
 
 loader
     .add([
@@ -85,7 +102,6 @@ loader
         "assets/img/sprites/banana.png",
         "assets/img/sprites/bread.png",
         "assets/img/sprites/broccoli.png",
-        "assets/img/sprites/egg.png",
         "assets/img/sprites/cd-1.png",
         "assets/img/sprites/cd-2.png",
         "assets/img/sprites/cd-3.png",
@@ -93,14 +109,7 @@ loader
         "assets/img/sprites/orange.png",
         "assets/img/sprites/play.png",
         "assets/img/web/site-logo-white-long.png",
-        "assets/img/sprites/obstacle.png",
-        "assets/img/tiling-sprites/sky.png",
-        "assets/img/tiling-sprites/mtn-far.png",
-        "assets/img/tiling-sprites/mtn-mid.png",
-        "assets/img/tiling-sprites/ground.png",
-        "assets/img/tiling-sprites/clouds.png",
-        "assets/img/tiling-sprites/trees.png",
-        "assets/img/tiling-sprites/grass.png"
+        "assets/img/sprites/obstacle.png"
     ])
     .on("progress", loadProgressHandler)
     .load(setup);
@@ -113,31 +122,38 @@ function initBackground() {
      sky | mtnFar | mtnMid | ground | clouds | trees | grass
      */
     sky =
-        new PIXI.extras.TilingSprite(resources['assets/img/tiling-sprites/sky.png'].texture, GAME_WIDTH, GAME_HEIGHT);
-    game.stage.addChild(sky);
+        new PIXI.extras.TilingSprite(PIXI.loader.resources.sky.texture,
+            GAME_WIDTH, GAME_HEIGHT);
+    stage.addChild(sky);
 
     mtnFar =
-        new PIXI.extras.TilingSprite(resources['assets/img/tiling-sprites/mtn-far.png'].texture, GAME_WIDTH, GAME_HEIGHT);
-    game.stage.addChild(mtnFar);
+        new PIXI.extras.TilingSprite(PIXI.loader.resources.mtnFar.texture,
+            GAME_WIDTH, GAME_HEIGHT);
+    stage.addChild(mtnFar);
 
     mtnMid =
-        new PIXI.extras.TilingSprite(resources['assets/img/tiling-sprites/mtn-mid.png'].texture, GAME_WIDTH, GAME_HEIGHT);
-    game.stage.addChild(mtnMid);
+        new PIXI.extras.TilingSprite(PIXI.loader.resources.mtnMid.texture,
+            GAME_WIDTH, GAME_HEIGHT);
+    stage.addChild(mtnMid);
 
     ground =
-        new PIXI.extras.TilingSprite(resources['assets/img/tiling-sprites/ground.png'].texture, GAME_WIDTH, GAME_HEIGHT);
-    game.stage.addChild(ground);
+        new PIXI.extras.TilingSprite(PIXI.loader.resources.ground.texture,
+            GAME_WIDTH, GAME_HEIGHT);
+    stage.addChild(ground);
 
     clouds =
-        new PIXI.extras.TilingSprite(resources['assets/img/tiling-sprites/clouds.png'].texture, GAME_WIDTH, GAME_HEIGHT);
-    game.stage.addChild(clouds);
+        new PIXI.extras.TilingSprite(PIXI.loader.resources.clouds.texture,
+            GAME_WIDTH, GAME_HEIGHT);
+    stage.addChild(clouds);
 
     trees =
-        new PIXI.extras.TilingSprite(resources['assets/img/tiling-sprites/trees.png'].texture, GAME_WIDTH, GAME_HEIGHT);
-    game.stage.addChild(trees);
+        new PIXI.extras.TilingSprite(PIXI.loader.resources.trees.texture,
+            GAME_WIDTH, GAME_HEIGHT);
+    stage.addChild(trees);
 
     grass =
-        new PIXI.extras.TilingSprite(resources['assets/img/tiling-sprites/grass.png'].texture, GAME_WIDTH, GAME_HEIGHT);
+        new PIXI.extras.TilingSprite(PIXI.loader.resources.grass.texture,
+            GAME_WIDTH, GAME_HEIGHT);
 
     // Prepare for first frame of game loop/animation
     lastTime = new Date().getTime();
@@ -157,59 +173,78 @@ function animateBackground() {
     trees.tilePosition.x -= BG_RATE * delta + backgroundScrollSpeed.trees;
     grass.tilePosition.x -= BG_RATE * delta + backgroundScrollSpeed.grass;
 
-    // Draw the game.stage and prepare for the next frame
+    // Draw the stage and prepare for the next frame
     lastTime = currtime;
+
 }
 
+function loadBackgroundTextures() {
+
+    if (window.devicePixelRatio >= 2 &&
+        renderer instanceof PIXI.WebGLRenderer) {
+        loader.add("sky", "assets/img/tiling-sprites/sky@2x.png");
+        loader.add("mtnFar", "assets/img/tiling-sprites/mtn-far@2x.png");
+        loader.add("mtnMid", "assets/img/tiling-sprites/mtn-mid@2x.png");
+        loader.add("ground", "assets/img/tiling-sprites/ground@2x.png");
+        loader.add("clouds", "assets/img/tiling-sprites/clouds@2x.png");
+        loader.add("trees", "assets/img/tiling-sprites/trees@2x.png");
+        loader.add("grass", "assets/img/tiling-sprites/grass@2x.png");
+    } else {
+        loader.add("sky", "assets/img/tiling-sprites/sky.png");
+        loader.add("mtnFar", "assets/img/tiling-sprites/mtn-far.png");
+        loader.add("mtnMid", "assets/img/tiling-sprites/mtn-mid.png");
+        loader.add("ground", "assets/img/tiling-sprites/ground.png");
+        loader.add("clouds", "assets/img/tiling-sprites/clouds.png");
+        loader.add("trees", "assets/img/tiling-sprites/trees.png");
+        loader.add("grass", "assets/img/tiling-sprites/grass.png");
+    }
+}
+
+
 /*
- Prints loading log to console.
+Prints loading log to console.
  */
 function loadProgressHandler() {
     console.log("loading");
 }
 
-var numberOfFood = 6;
+var apple = {name:"apple", weight:0.2};
+var banana = {name:"banana", weight:0.2};
+var bread = {name:"bread", weight:0.2};
+var orange = {name:"orange", weight:0.2};
+var broccoli = {name:"broccoli", weight:0.2};
 
-apple = {name: "apple", weight: 1 / numberOfFood};
-banana = {name: "banana", weight: 1 / numberOfFood};
-bread = {name: "bread", weight: 1 / numberOfFood};
-orange = {name: "orange", weight: 1 / numberOfFood};
-broccoli = {name: "broccoli", weight: 1 / numberOfFood};
-egg = {name: "egg", weight: 1 / numberOfFood};
-
-fallingObjects = [apple, banana, bread, orange, broccoli, egg];
+fallingObjects = [apple, banana, bread, orange, broccoli];
 
 /*
- Main game driver.
+Main game driver.
  */
 function setup() {
 
     // Initialize the the tiling-sprites background
     initBackground();
 
-    // Add sprites to game.stage
-    game.stage.addChild(grass);
+    // Add sprites to stage
+    stage.addChild(grass);
 
-    tk = new Tink(PIXI, game.renderer.view, scale);
+    tk = new Tink(PIXI, renderer.view, scale);
 
     //Touch and Mouse Controls
     pointer = tk.makePointer();
     //Pointer Definition
-    pointer.press = function () {
-    };
-    pointer.release = function () {
-    };
+    pointer.press = function () {};
+    pointer.release = function () {};
 
     setupdone = true;
 
     // Resize screen when window size is adjusted.
     window.addEventListener("resize", function (event) {
-        let scale = scaleToWindow(game.renderer.view);
+        let scale = scaleToWindow(renderer.view);
         tk.scale = scale;
     });
 
-    // Tell the 'game.renderer' to 'render' the 'game.stage'.
-    game.renderer.render(game.stage);
+    // Tell the 'renderer' to 'render' the 'stage'.
+    renderer.render(stage);
 
     //Start the game loop
     gameLoop();
@@ -227,7 +262,7 @@ function gameLoop() {
     state();
     lastTime = new Date().getTime();
     tk.update();
-    game.renderer.render(game.stage);
+    renderer.render(stage);
 }
 
 //State definition for "playing" the game
@@ -264,8 +299,8 @@ function gameMenuDisplay() {
         });
 
         // Add button and logo
-        game.stage.addChild(playButton);
-        game.stage.addChild(logo);
+        stage.addChild(playButton);
+        stage.addChild(logo);
 
         // Set game state indicators (e.i. has menu been built / has catcher been built)
         menuBuild = false;
@@ -275,8 +310,8 @@ function gameMenuDisplay() {
 
 function playGameFromMenu() {
     state = play;
-    game.stage.removeChild(playButton);
-    game.stage.removeChild(logo);
+    stage.removeChild(playButton);
+    stage.removeChild(logo);
 }
 
 
@@ -310,90 +345,8 @@ function initCatcher() {
 
         tk.makeDraggable(catcher);
 
-        game.stage.addChild(catcher);
+        stage.addChild(catcher);
 
         catcherBuild = false;
     }
-}
-
-function easterEgg() {
-
-    if (!easterEggEnabled) {
-
-        // create two render textures... these dynamic textures will be used to draw the scene into itself
-        renderTexture = PIXI.RenderTexture.create(
-            game.renderer.width,
-            game.renderer.height
-        );
-        renderTexture2 = PIXI.RenderTexture.create(
-            game.renderer.width,
-            game.renderer.height
-        );
-        currentTexture = renderTexture;
-
-        // create a new sprite that uses the render texture we created above
-        outputSprite = new PIXI.Sprite(currentTexture);
-
-        // align the sprite
-        outputSprite.x = 200;
-        outputSprite.y = 300;
-
-        // add to game.stage
-        game.stage.addChild(outputSprite);
-
-        stuffContainer = new PIXI.Container();
-
-        stuffContainer.x = GAME_WIDTH / 2;
-        stuffContainer.y = GAME_HEIGHT / 2;
-
-        game.stage.addChild(stuffContainer);
-
-        // now create some items and randomly position them in the stuff container
-        for (let i = 0; i < 100; i++) {
-            let item = new Sprite(
-                resources['assets/img/sprites/' + fallingObjects[i % fallingObjects.length].name + '.png'].texture
-            );
-            item.x = Math.random() * GAME_WIDTH - GAME_HEIGHT;
-            item.y = Math.random() * GAME_WIDTH - GAME_HEIGHT;
-            item.anchor.set(0.4);
-            stuffContainer.addChild(item);
-            items.push(item);
-        }
-
-        // used for spinning!
-        var count = 0;
-
-        easterEggEnabled = true;
-
-    }
-
-    function animateEasterEgg() {
-
-        for (let i = 0; i < items.length; i++) {
-            // rotate each item
-            let item = items[i];
-            item.rotation += 0.1;
-        }
-
-        count += 0.05;
-
-        // swap the buffers ...
-        let temp = renderTexture;
-        renderTexture = renderTexture2;
-        renderTexture2 = temp;
-
-        // set the new texture
-        outputSprite.texture = renderTexture;
-
-        // twist this up!
-        stuffContainer.rotation -= 0.01;
-        outputSprite.scale.set(1 + Math.sin(count) * 0.6);
-
-        // render the game.stage to the texture
-        // the 'true' clears the texture before the content is rendered
-        game.renderer.render(game.stage, renderTexture2, true);
-
-    }
-
-    animateEasterEgg();
 }
