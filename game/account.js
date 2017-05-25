ca6e9170a338280e426de4f91761e3eef17f3caa
kregan10 <kregan89@gmail.com>
2017-05-25 16:19:32 -0700

    fixed merge kerfufle

--- game/account.js
+++ game/account.js
@@ -40,7 +40,6 @@ const signUpAlert = document.getElementById('signupalert');
     const btnLogOutMainMenu = document.getElementById('btnLogOutMainMenu');
     const goToSignUp = document.getElementById('goToSignUp');
     const signInLink = document.getElementById('signinlink');
-    const fblogin = document.getElementById('btn-fblogin');
 
     // Text area select on touch
     txtEmail.addEventListener('touchend', function() {
@@ -69,28 +68,6 @@ const signUpAlert = document.getElementById('signupalert');
     signInLink.addEventListener('touchend', function() {
         $('#signupbox').hide(); $('#loginbox').show()
     });
-    
-    // Facebook login
-    fblogin.addEventListener('click', fbSignIn);
-    fblogin.addEventListener('touchend', fbSignIn);
-    function fbSignIn() {
-        var facebookProvider = new firebase.auth.FacebookAuthProvider();
-        facebookProvider.addScope('email');
-        firebase.auth().signInWithRedirect(facebookProvider).then(function (result) {
-            var token = result.credential.accessToken;
-            var user = result.user;
-
-            console.log(token);
-            console.log(user);
-            console.log(user.uid);
-        }).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-
-            console.log(errorCode);
-            console.log(errorMessage);
-        });
-    }
 
     // Event listeners for LOGIN button
     btnLogin.addEventListener('click', signIn);
@@ -166,6 +143,7 @@ const signUpAlert = document.getElementById('signupalert');
 
             loginAlert.style.display = "none";
             loginAlert.innerHTML = "";
+            displayScore(firebaseUser);
 
         } else {
             btnLogOut.classList.add('hide');
@@ -181,6 +159,18 @@ const signUpAlert = document.getElementById('signupalert');
     });
 
 }());
+
+function displayScore(user) {
+    var welcomeUserInfo = document.getElementById('welcomeUserInfo');
+    var userInDatabase = database.ref("users/" + user.uid);
+    // var welcomeUserInfo = database.ref("users/" + user.uid + "/userName");
+    userInDatabase.on('value', function(userSnapshot) {
+        console.log(userSnapshot.val());
+        welcomeUserInfo.innerHTML = "<p>Welcome <span class='inline-font'>" + userSnapshot.child("userName").val()
+            + "</span>, your highscore is <span style='color: #12ff19;' class='inline-font'>" + userSnapshot.child("score").val() + "</span></p>";
+    });
+}
+
 
 function showLoginAlert() {
     loginAlert.style.display = "block";
